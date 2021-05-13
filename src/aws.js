ef659f6fbb96add6f4393f00edc84e00790f92fd
Vladimir Stus <vladimir.stus@propulsuionapp.com>
2021-05-13 10:29:30 +0200

    removed console log

--- src/aws.js
+++ src/aws.js
@@ -29,7 +29,6 @@ const makeCreateOrUpdateSNSDestination = (ses) => async (name, configurationSet,
             }
         }
     };
-    console.log(params,'paramsSnsDestinationLog')
     const isAlreadyPresent = await isSNSDestinationAlreadyPresent(ses, configurationSet, name);
     if (isAlreadyPresent) {
         return ses.updateConfigurationSetEventDestination(params).promise();
